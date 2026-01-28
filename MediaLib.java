f4be4c9903043321a6824970428b4e9910d21fb7
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2026-01-28 12:03:03 -0800

    Update MediaLib.java

--- MediaLib.java
+++ MediaLib.java
@@ -1,1 +1,24 @@
 
+/**
+ * Activity 2.2.7
+ * 
+ * A MediaLib class for the MediaLibrary program
+ */
+public class MediaLib
+{
+  private Book book;
+
+  public void addBook(Book b)
+  {
+    book = b;
+  }
+/*Modify toString to show the state of the library 
+* @precondition: medialib not null
+*/
+
+  public String toString() 
+  {
+    String info = "Book added \n" + book;    
+    return info;
+  }
+}
